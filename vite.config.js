ba55f7acba56f8bc9fb55acbaae38c17cf4091c3
haekalrfny <haekalrfny@gmail.com>
2024-10-17 18:32:08 +0700

    idk

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: "/index.html",
-    },
+    outDir: "dist",
   },
   server: {
+    middlewareMode: true, 
     historyApiFallback: true,
   },
 });
