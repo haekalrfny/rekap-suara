c1415110c00a814cfb8f20c4d1e7e203912a6105
haekalrfny <haekalrfny@gmail.com>
2024-11-14 16:56:39 +0700

    change

--- src/pages/Paslon.jsx
+++ src/pages/Paslon.jsx
@@ -119,7 +119,11 @@ export default function Paslon() {
           <Switch menu={menuSwitch} value={type} setValue={setType} />
         </div>
       </div>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[90%]">
+      <div
+        className={`w-full grid grid-cols-1 md:grid-cols-2 ${
+          type === "pilgub" ? "lg:grid-cols-4" : "lg:grid-cols-5"
+        } gap-6 max-w-[90%]`}
+      >
         {displayedPaslon.map((item, index) => (
           <PaslonCard item={item} key={index} type={type} />
         ))}
